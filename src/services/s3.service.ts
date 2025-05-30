0bf2e258dee4ee697e2579d63a70dc85daccbcfc
Artur Bazhanau <a.bazhanov@dialmyapp.com>
2025-05-30 12:00:05 +0300

    add passthrough

--- src/services/s3.service.ts
+++ src/services/s3.service.ts
@@ -1,17 +1,9 @@
-import { S3Client } from '@aws-sdk/client-s3';
 import { Upload } from '@aws-sdk/lib-storage';
-import { Readable } from 'stream'
+import { Readable, PassThrough } from 'stream'
+import { s3 } from '../config/s3.config';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
-const s3 = new S3Client({
-  region: process.env.AWS_REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-  },
-});
 
 export const uploadPdfToS3 = async (
   fileBuffer: Buffer,
@@ -19,22 +11,21 @@ export const uploadPdfToS3 = async (
 ): Promise<string> => {
   const bucket = process.env.AWS_S3_BUCKET!;
   const key = `pdfs/${filename}`;
+  const passThrough = new PassThrough();
 
   const upload = new Upload({
     client: s3,
     params: {
       Bucket: bucket,
       Key: key,
-      Body: Readable.from(fileBuffer),
+      Body: passThrough,
       ContentType: 'application/pdf'
     },
     queueSize: 4,
     partSize: 5 * 1024 * 1024,
   })
 
-  upload.on('httpUploadProgress', (progress) => {
-    console.log(`Uploaded: ${progress.loaded}/${progress.total}`)
-  })
+  Readable.from(fileBuffer).pipe(passThrough);
 
   await upload.done()
 
